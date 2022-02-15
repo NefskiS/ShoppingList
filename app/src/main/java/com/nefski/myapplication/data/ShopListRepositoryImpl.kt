8665c581853d33cd395daaed2d4ebe43c3d7be67
NefskiS <Nefski@protonmail.com>
2022-02-15 12:25:48 +0300

    Finish

--- app/src/main/java/com/nefski/myapplication/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/nefski/myapplication/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.nefski.myapplication.domain.ShopItem
 import com.nefski.myapplication.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl : ShopListRepository {
 
@@ -12,6 +13,13 @@ object ShopListRepositoryImpl : ShopListRepository {
     private val shopList = sortedSetOf<ShopItem>({ p0, p1 -> p0.id.compareTo(p1.id) })
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
